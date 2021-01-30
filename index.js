12a91ee5aace2693f47d29e5e2618a157a1dca3d
swade781977 <swade781977@yahoo.com>
2021-01-30 16:16:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+
+function submitData(n, e){
+    let data = {
+        name: n,
+        email: e
+    }
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(data)
+    }
+    return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            document.body.innerHTML = object[ "id" ]
+        })
+        .catch(function(error) {
+            alert("UH OH");
+            document.body.innerHTML = error.message
+        });
+}
